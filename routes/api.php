ea1b79f95cb2251a69fb49468f9a2ee4284d7320
Abraham Chuljyan <abraham.chuljyan98@gmail.com>
2022-06-21 15:51:26 +0400

    changed unsubscribe method type

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ $router->group(['prefix' => 'api/v1'], function () use ($router) {
     $router->get('/', 'MainController@main');
 
     $router->post('/sendEmail', 'WaitListController@sendEmail');
-    $router->get('/unsubscribe/{id}', 'WaitListController@unsubscribe');
+    $router->post('/unsubscribe/{id}', 'WaitListController@unsubscribe');
 
     $router->get('/login-message', 'Web3AuthController@message');
     $router->post('/login-verify', 'Web3AuthController@verify');
