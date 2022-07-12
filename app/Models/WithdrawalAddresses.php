affb3f54a6c99d318ab05aad3b160529c9837001
Abraham Chuljyan <abraham.chuljyan98@gmail.com>
2022-07-12 14:51:14 +0400

    fixed wthdrawal addresses bug

--- app/Models/WithdrawalAddresses.php
+++ app/Models/WithdrawalAddresses.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Laravel\Lumen\Auth\Authorizable;
 
 class WithdrawalAddresses extends Model implements AuthenticatableContract, AuthorizableContract
@@ -29,4 +30,18 @@ class WithdrawalAddresses extends Model implements AuthenticatableContract, Auth
         'user_id', 'contract_id', 'addresses'
     ];
 
+    /**
+     * @param array $data
+     * @return bool
+     */
+    public static function create(array $data): bool {
+        return DB::table('withdrawal_addresses')->insert([
+            'user_id' => $data['user_id'],
+            'contract_id' => $data['contract_id'],
+            'percent' => $data['percent'],
+            'address' => $data['address'],
+            'created_at' => date('Y-m-d H:i:s', time()),
+            'updated_at' => date('Y-m-d H:i:s', time())
+        ]);
+    }
 }
