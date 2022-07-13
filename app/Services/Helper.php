77e73cb3c85e70aafa47639a72beb5e077518f93
Abraham Chuljyan <abraham.chuljyan98@gmail.com>
2022-07-13 17:54:30 +0400

    fixed bytecode

--- app/Services/Helper.php
+++ app/Services/Helper.php
@@ -24,4 +24,13 @@ class Helper
 
         return $input;
     }
+
+    public static function getStringBetween($string, $start, $end){
+        $string = ' ' . $string;
+        $ini = strpos($string, $start);
+        if ($ini == 0) return '';
+        $ini += strlen($start);
+        $len = strpos($string, $end, $ini) - $ini;
+        return substr($string, $ini, $len);
+    }
 }
